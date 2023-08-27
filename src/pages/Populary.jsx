b786b41927663365887cb3d2b2b35df4d265b225
AlexP00 <Petkovic.Alex.0@gmail.com>
2023-08-27 16:52:09 +0200

    add populary page and pupulary style

---
+++ src/pages/Populary.jsx
@@ -0,0 +1,131 @@
+import '../styles/populary.scss';
+
+const Populary = () => {
+    return (
+        <main className="populary-content">
+            <h1>Populary Foods <span>...</span></h1>
+            <div className="food">
+            <div className="image">
+                <img src="https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/w_1000,h_684,c_limit/the-ultimate-hamburger.jpg"
+                 alt="hamburger" />
+            </div>
+            <div className="info">
+                <h3>Hamburger</h3>
+                <p>Hamburger (abbreviated burger) is a type of sandwich in a round bun, and the same word can also be
+                     used for sliced ​​beef steak, which is most often placed in a bun when making such a sandwich.
+                   Meat steak, which is the basic ingredient of a hamburger, is usually accompanied by vegetables such 
+                   as lettuce, onions and pickles, as well as various sauces such as ketchup, mustard or mayonnaise, and
+                    hamburgers in which a few slices of cheese are added to the previously mentioned ingredients are also 
+                    popular. Hamburger with cheese is also called "cheeseburger", since "cheese" means cheese in English.
+                    </p>
+                    <p className='origin'>Origin : <span>SAD</span></p>
+            </div>
+            </div>
+            <div className="food">
+            <div className="image">
+                <img src="https://www.thespruceeats.com/thmb/lmDJraajDXMJ9izsIfzNt79GrSs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Pizzasupremehoriz-1ccfa0b1732b4c128427d19ae02a422b.jpg"
+                 alt="pizza" />
+            </div>
+            <div className="info">
+                <h3>Pizza</h3>
+                <p>Pizza (ital. pizza) is a specialty of Italian cuisine, usually made of thin,
+                     round dough on which, in addition to tomato sauce, you can find different types 
+                     of cheese, meat, toppings, and fruit. The inventiveness of the taste can be complemented
+                      by the inventiveness of the layout composition on the pizza, which can be arranged in the
+                       form of ornaments or other interesting figures. By the way, this dish is considered particularly
+                        characteristic of Neapolitan cuisine. Today, pizza is very popular in the world, primarily because 
+                        of its cheap and easily available ingredients and ease of preparation.
+                    </p>
+                    <p className='origin'>Origin : <span>Italia</span></p>
+            </div>
+            </div>
+             <div className="food">
+            <div className="image">
+                <img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.616.493.suffix/1615916524567.jpeg"
+                 alt="pasta" />
+            </div>
+            <div className="info">
+                <h3>Pasta</h3>
+                <p>Pasta or pasta is a group name for all specialties made from cut or dried dough, prepared from flour,
+                     eggs, salt and water. It is eaten all over the world, and it is assumed that its birthplace is Italy 
+                     or China. In Bosnia and Herzegovina, the collective noun macaroni is incorrectly used colloquially, 
+                     although it refers to noodles or pasta of all possible shapes and forms. Pasta is originally eaten boiled
+                      in salted water, and at the end of cooking a previously prepared sauce is added to it or the sauce is 
+                      cooked with the pasta until it is ready. Pasta is the main food of Italian cuisine.
+                    </p>
+                    <p className='origin'>Origin : <span>Italia</span></p>
+            </div>
+            </div> 
+            <div className="food">
+            <div className="image">
+                <img src="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0="
+                 alt="Sushi" />
+            </div>
+            <div className="info">
+                <h3>Sushi</h3>
+                <p>Sushi is the name for a Japanese dish that consists of two essential parts: boiled rice with
+                     the addition of rice vinegar (shari) and rice additives (neta). Neta toppings (mostly fish
+                      and seafood) as well as the shape of the sushi can vary a lot; however, one thing that all s
+                      ushi has in common is chari (rice with vinegar). Serving only fresh and raw meat is called 
+                      sashimi, and is a completely different dish from sushi.
+
+                    </p>
+                    <p className='origin'>Origin : <span>Japan</span></p>
+            </div>
+            </div>
+             <div className="food">
+            <div className="image">
+                <img src="https://walter.rs/wp-content/uploads/2022/06/VM25796-2.jpg"
+                 alt="hamburger" />
+            </div>
+            <div className="info">
+                <h3>Cevapi</h3>
+                <p>Ćevapcici or kebabs are a dish made from minced meat, shaped into small rolls and grilled. 
+                    Most often, they are made from mixed beef (70%) and sheep (30%) meat. They occur in all 
+                    parts of the Balkans, especially in the former Yugoslavia where the Turks ruled. The types 
+                    of meat differ from place to place, and often from the religion that prevails in a region.
+                     They can also be found as a type of fast food in bread and with side dishes (most often onions and cream).
+                    </p>
+                    <p className='origin'>Origin : <span>Serbia</span></p>
+            </div>
+            </div>
+            <div className="food">
+            <div className="image">
+                <img src="https://becs-table.com.au/wp-content/uploads/2014/01/ice-cream-1.jpg"
+                 alt="hamburger" />
+            </div>
+            <div className="info">
+                <h3>Ice-Cream</h3>
+                <p>Ice cream is a frozen dessert typically made from milk or cream that has been flavoured with a sweetener,
+                     either sugar or an alternative, and a spice, such as cocoa or vanilla, or with fruit, such as strawberries
+                      or peaches. Food colouring is sometimes added in addition to stabilizers. The mixture is cooled below the
+                       freezing point of water and stirred to incorporate air spaces and prevent detectable ice crystals from forming. 
+                       It can also be made by whisking a flavoured cream base and liquid nitrogen together. 
+                    </p>
+                    <p className='origin'>Origin : <span>SAD</span></p>
+            </div>
+            </div>
+            <div className="food">
+            <div className="image">
+                <img src="https://www.culinaryhill.com/wp-content/uploads/2022/04/Beef-Gyros-Culinary-Hill-1200x800-1-500x500.jpg"
+                 alt="hamburger" />
+            </div>
+            <div className="info">
+                <h3>Gyros</h3>
+                <p>Gyros is a traditional Greek dish of meat grilled on a vertical spit,
+                     usually served as a sandwich, with tomato, onion and tzatziki sauce,
+                      wrapped in pita bread (the Greek version of a thin round bread, or bun).
+                      Да би се испекли, комади меса се налазе на високом вертикалном ражњу, који
+                       се окреће испред извора топлоте, обично електричног грејача. Ако месо није довољно масно,
+                        траке сланине се додају тако да печено месо увек остаје сочно и хрскаво.
+                    </p>
+                    <p className='origin'>Origin : <span>Greece</span></p>
+            </div>
+            </div>
+        </main>
+    )
+
+    
+}
+ 
+export default Populary;
