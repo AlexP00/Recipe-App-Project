4780145bd520ac4821495ff34fe718c6599abfe6
AlexP00 <Petkovic.Alex.0@gmail.com>
2023-08-28 21:08:26 +0200

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 // Components
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
+import Recipe from './pages/Recipe';
+import Populary from './pages/Populary';
+import Recipes from './pages/Recipes';
 
 // Other
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 //styles
 import './styles/index.scss';
@@ -14,6 +19,9 @@ function App() {
        <NavBar /> 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/recipe' element={<Recipe /> } />
+        <Route  path='/populary' element={<Populary />}/>
+        <Route path='/recipes/:id' element={<Recipes />}/>
       </Routes>
     </Router>
   </main>
